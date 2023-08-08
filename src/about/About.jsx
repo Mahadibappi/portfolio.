f10e8cfcead3b7a3c26304da3543bc835a21ec35
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-08-08 10:40:28 +0600

    about section added

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./about.css";
+import profile from "../assets/profile-2.png";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_image">
+            <img src={profile} alt="" />
+          </div>
+        </div>
+        <div className="about_content">
+          <article>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda,
+            at in eum corrupti nisi eaque neque labore asperiores voluptates
+            minima. Maiores eligendi quidem praesentium sed quia iusto aut
+            repellendus ut eius eaque optio ducimus, accusantium odio alias esse
+            adipisci, dolorum atque quo numquam! Molestias fuga esse numquam cum
+            dolores. Atque!
+          </article>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBookHeart } from "react-icons/bi";
 import { BiSolidMessageDetail } from "react-icons/bi";
 const Nav = () => {
+  const [active, setActive] = useState("#");
   return (
-    <div>
-      <a href="#">
+    /* eslint-disable jsx-a11y/anchor-is-valid */
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActive("#")}
+        className={active === "#" ? "active" : " "}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActive("#about")}
+        className={active === "#about" ? "active" : " "}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#skills">
+      <a
+        href="#skills"
+        onClick={() => setActive("#skills")}
+        className={active === "#skills" ? "active" : " "}
+      >
         <BiBookHeart />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActive("#contact")}
+        className={active === "#contact" ? "active" : " "}
+      >
         <BiSolidMessageDetail />
       </a>
-    </div>
+    </nav>
   );
 };
 
